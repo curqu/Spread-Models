b791068043210ac89b482bde11196fc32600bea8
Carla Urquhart <44617352+curqu@users.noreply.github.com>
2019-05-10 10:26:48 -0700

    complete basic dispersal

--- pop_growth_model.R
+++ pop_growth_model.R
@@ -1,63 +1,123 @@
 # model dispersal
 
+#########################################################
+
 # parameters
 
 #intrinsic rate of increase 
-lambda <- 10 #try different values
+lambda <- 10
 #intraspecific competition
-alpha <- 1 #try different values
+alpha <- .1
 #distance decay rate
-m <- 0.3 #try different values
+m <- 0.3
 #starting population
-N_0 <- 30 #try different values
+N_0 <- 100
 #number of generations to simulate
-g_max <- 10 #unused so far, need to think about how to automate generations
+g_max <- 10
 #number of patches
 patch<-10
 #distance between patches vector
-dpatch<- 0.1 #I think that this method should incorporate patchiness, since each distance cell is a kind of box?
-#if I can figure out how to vary this across the landscape, could serve as a proxy for fuzziness? ie. if the distances vector is 
-#(0.1, 0.2, 0.3, 1, 2, 3, 3.1, 3.2, 3.3)
+dpatch<- 0.1
+#Define distance vector
+dvect<-seq(from=0,length.out = patch,by=dpatch)
+
+##########################################################
 
 #functions
 
-#seed production function (from Papchepsky and Levine)
+#seed production function
+#WHY DOES IT MAKE LESS SEEDS THAN POP? (Parameters, I think?)
 gNty <- function(j){
+  seeds<-vector()
     jnext<-(lambda*j)/(1+alpha*j)
-    return(jnext)
+    seeds<-c(seeds,jnext)
+    return(seeds)
 }
 
-#dispersal kernel (from Pachepsky and Levine) 
-#this might be off, probabilities adding up to more than 1?
+#dispersal kernel
 ker <- function(x,y){
   k <-(m/2)*exp(-m*abs(x-y))
   return(k)
 }
+  
+##########################################################
 
-#Population density
-Ntnextx <- #probably should delete this function
+#Construct Probability Matrix for Dispersal
+
+#Rows are starting patch
+#Coloumns are colonized patch
+#vector of probabilities
+dprobv<-vector()
+for (i in dvect){
+  for (j in dvect){
+    pd<-ker(i,j)
+    dprobv<-c(dprobv,pd)
+  }
+}
+
+#Convert to Matrix
+dprob<-matrix(dprobv,nrow = patch, ncol = patch) 
+
+##########################################################
+
+#Initialize Population Matrix
+
+#Rows are timesteps
+#Coloumns are Patches -- Patch i corresponds to ith distance in dvect
+N_i<-matrix(0,1,patch)
+N_i[1,1]<-N_0
+
+##########################################################
+
+#Run for time t_i
+
+#Set Current timestep
+Nt_i<-tail(N_i,1)
+
+#Produce Seeds
+seedsv<-gNty(Nt_i)
+
+#Disperse Seeds 
+#match seedsv with originating row in dprob
+disp<-vector()
+for (i in 1:patch){
+  for (j in 1:patch){
+    dfromi<-seedsv[i]*dprob[i,j]
+    disp<-c(disp,dfromi)
+  }
+}
+#Convert to matrix
+#coloumns correspond to originating patches
+#rows are destination patch
+disp<-matrix(disp,patch,patch)
+
+#calculate dispersal for each entry
+#sum total seeds
+tdisp<-vector()
+for (i in 1:patch){
+  drt<-sum(disp[i,])
+  tdisp<-c(tdisp,drt)
+}
+
+#populate next row of N_i
+N_i<-rbind(N_i,tdisp)
 
 
-#Define Starting Matrix -- row 1 is distance vector, row 2 is population, row 3 is seeds produced (should reinitialize back to zero after each generation)
-N_i <- matrix(0,3,patch)
+#OLD WAY BUT WRONG WAY???
+#Define Starting Matrix -- row 1 dist, 2 pop., 3 seeds
+#N_i <- matrix(0,3,patch)
 #Initialize starting pop
-N_i[2,1]<-N_0
-#Define distance vector
-dvect<-seq(from=0,length.out = patch,by=dpatch)
-N_i[1,]<-dvect
+#N_i[2,1]<-N_0
+
+#N_i[1,]<-dvect
 
 #produce seeds
 
-for(j in N_i[2,]){
-  N_i[3,] <- gNty(N_i[2,])
-}
+#for(j in N_i[2,]){
+  # N_i[3,] <- gNty(N_i[2,])
+# }
 
 #disperse seeds
-
-#tried to do it all in one step, but this is probably not a good idea, instead made probability matrix,
-#need to define a function that will disperse seeds in [3,i] to cells [2,j] based on value in dprob[i,j]
-#something like N_i[2,j]=N_i[3,i]*dprob[i,j]+N_i[2,j], looping somehow, how to record coloumn numbers in loop?
-#try: change distance values back to patch #, use distance vector only to create dprob, or add row for patch#?
 
 #for (i in col(N_i)){
 #  for (j in N_i[1,]){
@@ -74,16 +134,8 @@ for(j in N_i[2,]){
 #  }
 #}
 
-#move this to initialization section, dprob is constant
-#change N_i[1,] to dvect? then N_i[1,] should be indices of patches [1:patch]
-dprobv<-vector()
-for (i in N_i[1,]){
-  for (j in N_i[1,]){
-    pd<-ker(i,j)
-    dprobv<-c(dprobv,pd)
-  }
-}
-
-dprob<-matrix(dprobv,nrow = patch, ncol = patch) 
-
   
+# for (i in y){
+#   pd<-ker(1,i)
+#   density<-c(density,pd)
+# }
